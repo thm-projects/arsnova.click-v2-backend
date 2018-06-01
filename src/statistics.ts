1912574d114056740585dd98893222f0adf213bf
Christopher Fullarton <christopher@fullarton.eu>
2018-06-01 15:18:18 +0200

    Fixes path to assets for the i18n file location

--- src/statistics.ts
+++ src/statistics.ts
@@ -59,5 +59,5 @@ export const projectAppLocation = {
   'arsnova-click-v2-backend': path.join(projectBaseLocation['arsnova-click-v2-backend']),
 };
 export const i18nFileBaseLocation = {
-  'arsnova-click-v2-backend': path.join(projectBaseLocation['arsnova-click-v2-backend'], 'assets', 'i18n'),
+  'arsnova-click-v2-backend': path.join(staticStatistics.pathToAssets, 'i18n'),
 };
