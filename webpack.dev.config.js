6f76afd80b740b7fc02e8ae176a40d4bc2896982
Christopher Fullarton <christopher@fullarton.eu>
2018-06-12 15:39:53 +0200

    Fixes usage of env variables during the build process

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -26,14 +26,24 @@ const config = {
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
-        "BUILD_TARGET": JSON.stringify('server')
-      }
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+      ARSNOVA_CLICK_BACKEND_PORT_INTERNAL: process.env.ARSNOVA_CLICK_BACKEND_PORT_INTERNAL,
+      ARSNOVA_CLICK_BACKEND_PORT_EXTERNAL: process.env.ARSNOVA_CLICK_BACKEND_PORT_EXTERNAL,
+      ARSNOVA_CLICK_BACKEND_ROUTE_PREFIX: process.env.ARSNOVA_CLICK_BACKEND_ROUTE_PREFIX,
+      ARSNOVA_CLICK_BACKEND_REWRITE_ASSET_CACHE_URL: process.env.ARSNOVA_CLICK_BACKEND_REWRITE_ASSET_CACHE_URL,
+      ARSNOVA_CLICK_BACKEND_SMTP_HOST: process.env.ARSNOVA_CLICK_BACKEND_SMTP_HOST,
+      ARSNOVA_CLICK_BACKEND_SMTP_USERNAME: process.env.ARSNOVA_CLICK_BACKEND_SMTP_USERNAME,
+      ARSNOVA_CLICK_BACKEND_SMTP_PASSWORD: process.env.ARSNOVA_CLICK_BACKEND_SMTP_PASSWORD,
+      ARSNOVA_CLICK_BACKEND_SMTP_PORT: +process.env.ARSNOVA_CLICK_BACKEND_SMTP_PORT,
+      ARSNOVA_CLICK_BACKEND_MAIL_FROM: process.env.ARSNOVA_CLICK_BACKEND_MAIL_FROM,
+      ARSNOVA_CLICK_BACKEND_MAIL_TO: process.env.ARSNOVA_CLICK_BACKEND_MAIL_TO,
     }),
     new CopyWebpackPlugin([
-      { from: 'assets', to: 'assets' }
+      {from: 'assets', to: 'assets'}
     ]),
     new CopyWebpackPlugin([
-      { from: 'jobs', to: 'jobs' }
+      {from: 'jobs', to: 'jobs'}
     ]),
   ],
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,49 +2,52 @@ const base = require("./webpack.base.config");
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const GenerateJsonPlugin = require('generate-json-webpack-plugin');
-
-const path = require('path');
-
-const devPackagejson = require('./package');
-delete devPackagejson.devDependencies;
-delete devPackagejson.scripts;
-
-devPackagejson.scripts = {'start': 'node server.js'};
-
-const buildInfoJson = require('./build');
-const newBuild = parseInt(buildInfoJson.build, 10) + 1;
-const buildInfo = Object.assign(buildInfoJson, {
-  build: newBuild,
-  version: `${buildInfoJson.major}.${buildInfoJson.minor}.${newBuild}`,
-  timestamp: new Date().getTime()
-});
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 const config = {
   entry: [
+    'webpack/hot/poll?1000',
     './src/main.ts'
   ],
+  devServer: {
+    port: 3000,
+    inline: true,
+    hot: true,
+    contentBase: '/dist',
+    watchContentBase: true
+  },
   externals: [nodeExternals({
     whitelist: ['webpack/hot/poll?1000']
   })],
+  devtool: 'source-map',
   plugins: [
     new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
-        "BUILD_TARGET": JSON.stringify('server')
-      }
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+      ARSNOVA_CLICK_BACKEND_PORT_INTERNAL: process.env.ARSNOVA_CLICK_BACKEND_PORT_INTERNAL,
+      ARSNOVA_CLICK_BACKEND_PORT_EXTERNAL: process.env.ARSNOVA_CLICK_BACKEND_PORT_EXTERNAL,
+      ARSNOVA_CLICK_BACKEND_ROUTE_PREFIX: process.env.ARSNOVA_CLICK_BACKEND_ROUTE_PREFIX,
+      ARSNOVA_CLICK_BACKEND_REWRITE_ASSET_CACHE_URL: process.env.ARSNOVA_CLICK_BACKEND_REWRITE_ASSET_CACHE_URL,
+      ARSNOVA_CLICK_BACKEND_SMTP_HOST: process.env.ARSNOVA_CLICK_BACKEND_SMTP_HOST,
+      ARSNOVA_CLICK_BACKEND_SMTP_USERNAME: process.env.ARSNOVA_CLICK_BACKEND_SMTP_USERNAME,
+      ARSNOVA_CLICK_BACKEND_SMTP_PASSWORD: process.env.ARSNOVA_CLICK_BACKEND_SMTP_PASSWORD,
+      ARSNOVA_CLICK_BACKEND_SMTP_PORT: +process.env.ARSNOVA_CLICK_BACKEND_SMTP_PORT,
+      ARSNOVA_CLICK_BACKEND_MAIL_FROM: process.env.ARSNOVA_CLICK_BACKEND_MAIL_FROM,
+      ARSNOVA_CLICK_BACKEND_MAIL_TO: process.env.ARSNOVA_CLICK_BACKEND_MAIL_TO,
     }),
-    new GenerateJsonPlugin('package.json', devPackagejson),
-    new GenerateJsonPlugin('build.json', buildInfo),
-    new GenerateJsonPlugin(path.join('..', 'build.json'), buildInfo),
     new CopyWebpackPlugin([
-      { from: 'assets', to: 'assets' }
+      {from: 'assets', to: 'assets'}
     ]),
     new CopyWebpackPlugin([
-      { from: 'jobs', to: 'jobs' }
+      {from: 'jobs', to: 'jobs'}
     ]),
   ],
 };
+
+//config.plugins.push(new WebpackShellPlugin({onBuildEnd: ['node dist/main.js --inspect=9229 --inspect-brk dist/main.js']}));
 
 module.exports = base.extend(config);
