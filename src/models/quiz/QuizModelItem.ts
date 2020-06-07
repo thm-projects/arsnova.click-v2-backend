437243378751f7cac0359c3732d0d79c1af3dde1
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2020-06-07 14:24:07 +0200

    Includes the iquiz interface for the quizmodelitem

--- src/models/quiz/QuizModelItem.ts
+++ src/models/quiz/QuizModelItem.ts
@@ -6,6 +6,7 @@ import { DbCollection } from '../../enums/DbOperation';
 import { QuizState } from '../../enums/QuizState';
 import { QuizVisibility } from '../../enums/QuizVisibility';
 import { IQuestion } from '../../interfaces/questions/IQuestion';
+import { IQuiz } from '../../interfaces/quizzes/IQuizEntity';
 import { SessionConfigurationModelItem } from '../session-config/SessionConfigurationModelItem';
 
 @index({ name: 1 }, {
@@ -15,7 +16,7 @@ import { SessionConfigurationModelItem } from '../session-config/SessionConfigur
     strength: 1,
   },
 })
-export class QuizModelItem {
+export class QuizModelItem implements IQuiz {
   @prop({ required: false }) @IsDate() public expiry?: Date;
   @prop({
     required: false,
